b1d290d011e5f66a395c124658ab6b164b36dd6e
pKm69 <priyanshumahato0210@gmail.com>
2025-10-18 01:30:43 +0530

    initialized sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Video from './Pages/Video/Video'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+      </Routes>
     </div>
   )
 }
